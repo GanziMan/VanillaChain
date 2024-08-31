8344fcbde7b9ed559d4ce10b0bf9ed24697ebe30
GanziMan <qjatn50089@gmail.com>
2024-08-31 23:06:25 +0900

    ADD: server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+// 모든 요청에 대해 index.html 파일을 반환합니다.
+app.use(
+  "/static",
+  express.static(path.resolve(__dirname, "frontend", "static"))
+);
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontEnd", "index.html"));
+});
+
+// 5060 포트에서 서버를 실행합니다.
+app.listen(4000, () => console.log("Server running on port 5060"));
